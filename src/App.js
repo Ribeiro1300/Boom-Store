c1a4ff4779da9a00d72056d3191a0597488112c3
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-11-06 18:51:33 -0300

    basic pages, done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import Products from "./pages/Products";
+import SingleProduct from "./pages/SingleProduct";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
-    <div>
-      <h1>Olá</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/product/:product_id" exact>
+          <SingleProduct />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
