1cf4784eed49256e1ee8be6be19cd81791120775
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-11-14 12:05:46 -0300

    getting product info from database and basic layout

--- src/pages/SingleProduct/index.js
+++ src/pages/SingleProduct/index.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Content } from "../Products/styles";
+import * as S from "./styles";
 
 export default function SingleProduct() {
-  return <div>olá</div>;
+  const [productInfo, setProductInfo] = useState({});
+  const id = useParams().product_id;
+  useEffect(async () => {
+    try {
+      const result = await axios.get(
+        "http://localhost:4000/singleProduct/" + id
+      );
+      if (result.status === 201) {
+        setProductInfo(result.data[0]);
+      }
+    } catch (error) {
+      alert("Algo deu errado, tente recarregar a página");
+    }
+  }, []);
+  return (
+    <Content>
+      <S.SingleProductWrapper>
+        <h1>{productInfo.name}</h1>
+        <img src={productInfo.img}></img>
+        <S.PriceInfo>{productInfo.price.toFixed(2)}</S.PriceInfo>
+      </S.SingleProductWrapper>
+      <S.Description></S.Description>
+    </Content>
+  );
 }
---
+++ src/pages/SingleProduct/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const SingleProductWrapper = styled.div`
+  width: 80%;
+  height: 80vh;
+  background-color: white;
+  margin-top: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: flex-start;
+
+  h1 {
+    margin: 10px;
+    width: 100%;
+  }
+
+  img {
+    width: 400px;
+  }
+`;
+
+export const PriceInfo = styled.div``;
+
+export const Description = styled.div`
+  width: 80%;
+  height: 80vh;
+  background-color: white;
+  margin-top: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
