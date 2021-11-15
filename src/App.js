8d5dc153bd33d09b60f1d320fc399fbd53220a59
fernandollisboa <fernando.costa@ccc.ufcg.edu.br>
2021-11-15 20:14:47 -0300

    add: starting addtocart

--- src/App.js
+++ src/App.js
@@ -12,31 +12,29 @@ import ProtectedRoute from "./utils/ProtectedRoute";
 
 function App() {
   return (
-    <Style>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact>
-            <Products />
-          </Route>
-          <Route path="/login" exact>
-            <Login />
-          </Route>
-          <ProtectedRoute path="/profile" exact>
-            <Profile />
-          </ProtectedRoute>
-          <Route path="/cart" exact>
-            <Cart />
-          </Route>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>
-          <Route path="/:product_id" exact>
-            <SingleProduct />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </Style>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Products />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <ProtectedRoute path="/profile" exact>
+          <Profile />
+        </ProtectedRoute>
+        <Route path="/cart" exact>
+          <Cart />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/:product_id" exact>
+          <SingleProduct />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
