737acaba178b510c21dff7e06d9fd7ac672e83c4
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-11-15 20:48:46 -0300

    fix singleProduct route

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           <Route path="/checkout">
             <Checkout />
           </Route>
-          <Route path="/:product_id" exact>
+          <Route path="/products/:product_id" exact>
             <SingleProduct />
           </Route>
         </Switch>
