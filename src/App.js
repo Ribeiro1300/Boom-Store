c76933c68ac494c980d0ac92554cef37ff86e6da
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-11-15 21:39:04 -0300

    deleting AppStyles

--- src/App.js
+++ src/App.js
@@ -6,37 +6,34 @@ import Products from "./pages/Products";
 import SingleProduct from "./pages/SingleProduct";
 import Checkout from "./pages/Checkout";
 import Navbar from "./components/Navbar";
-import Style from "./AppStyle";
 import Profile from "./pages/Profile";
 import ProtectedRoute from "./utils/ProtectedRoute";
 
 function App() {
   return (
-    <Style>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact>
-            <Products />
-          </Route>
-          <Route path="/login" exact>
-            <Login />
-          </Route>
-          <ProtectedRoute path="/profile" exact>
-            <Profile />
-          </ProtectedRoute>
-          <Route path="/cart" exact>
-            <Cart />
-          </Route>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>
-          <Route path="/products/:product_id" exact>
-            <SingleProduct />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </Style>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Products />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <ProtectedRoute path="/profile" exact>
+          <Profile />
+        </ProtectedRoute>
+        <Route path="/cart" exact>
+          <Cart />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/products/:product_id" exact>
+          <SingleProduct />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
